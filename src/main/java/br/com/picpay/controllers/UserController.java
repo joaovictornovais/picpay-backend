7f1ee436962fd6ddc7f36c58facfd69c8f17108b
João Victor Novais <joao.dipaay@gmail.com>
2023-08-15 14:28:56 -0300

    feat: Controller User

---
+++ src/main/java/br/com/picpay/controllers/UserController.java
@@ -0,0 +1,31 @@
+package br.com.picpay.controllers;
+
+import br.com.picpay.domain.user.User;
+import br.com.picpay.dtos.UserDTO;
+import br.com.picpay.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody UserDTO userDTO) {
+        User newUser = userService.createUser(userDTO);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+}
