3dfddbed298ecb911a69f791f69d5d091f8162ae
João Victor Novais <joao.dipaay@gmail.com>
2023-08-15 14:29:05 -0300

    feat: Controller Transaction

---
+++ src/main/java/br/com/picpay/controllers/TransactionController.java
@@ -0,0 +1,25 @@
+package br.com.picpay.controllers;
+
+import br.com.picpay.domain.transaction.Transaction;
+import br.com.picpay.dtos.TransactionDTO;
+import br.com.picpay.services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping("/transactions")
+public class TransactionController {
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<Transaction> createTransaction(@RequestBody TransactionDTO transactionDTO) throws Exception {
+        Transaction newTransaction = transactionService.createTransaction(transactionDTO);
+        return new ResponseEntity<>(newTransaction, HttpStatus.OK);
+    }
+}
